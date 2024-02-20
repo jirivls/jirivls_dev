3b3df751deff58be7353b8f253a4874ec96dc760
Jiří Voleský <voleskyjirka@gmail.com>
2024-02-20 08:26:12 +0100

    new method

---
+++ ForkTest/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkTest
+{
+    internal class Class1
+    {
+        public static int Test(int x)
+        {
+            return x;
+        }
+    }
+}
