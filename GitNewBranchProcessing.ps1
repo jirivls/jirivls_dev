8ba0324f62c1cf863b5073a7a99866ac6e176580
jiri.volesky <voleskyjirka@gmail.com>
2025-05-13 13:55:37 +0200

    GitNewBranchProcessing.ps1 odstraneni input param, nacitani z config

--- GitNewBranchProcessing.ps1
+++ GitNewBranchProcessing.ps1
@@ -1,9 +1,31 @@
-param (
-    [string]$BranchName # Name of the new branch
-)
+# Ziskani root cesty repository
+$gitRoot = git rev-parse --show-toplevel
+
+# Slozeni cesty k config.json
+$configPath = Join-Path $gitRoot "Tools.Binaries\Publisher\config.json"
+
+# Precteni configu
+$config = Get-Content $configPath -Raw | ConvertFrom-Json
 
 # Nastaveni API tokenu
 $gitToken = "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"
+
+# Vytazeni posledni verze
+$fullVersion = $config.DMS.LastVersion
+
+# Nacteni bez build verze
+$segments = ($fullVersion -split '\.')
+
+# Zvyseni PATCH verze o 1
+$segments[2] = ([int]$segments[2]) + 1
+
+# Poskladani verze zpet bez build cisla
+$nextVersion = ($segments[0..2] -join '.')
+
+Write-Host "New version (auto-incremented): $nextVersion"
+
+# Pouzije se jako jmeno pro novou branch i slozku
+$BranchName = $nextVersion
 
 # Prepnuti do Develop branch
 git checkout Develop
@@ -22,31 +44,22 @@ Write-Host "Remote repository URL: $repoUrl"
 $authGitUrl = $repoUrl -replace "https://", "https://$gitToken@"
 
 # Naklonovani branch na lokal
-$cloneDir = $BranchName -replace "\.", ""
+$parentDir = Split-Path $gitRoot -Parent
+$cloneDir = Join-Path $parentDir $BranchName
 Write-Host "Cloning repository with only branch $BranchName..."
 git clone --single-branch --branch $BranchName $authGitUrl $cloneDir
 
 # Prepnuti do nove branch
 Set-Location $cloneDir
 
-# Natahnuti vsech remote branch pro novou
-Write-Host "Fetching all remote branches..."
-git fetch --all
-Write-Host "Setting remote-tracking branches..."
-git remote set-branches origin "*"
-git fetch origin
-git branch -r
-
-# Prepnuti zpet do Develop repository
-Set-Location ..
+# Prepnuti zpet do Develop branch
+Set-Location $gitRoot
 
 # Odstraneni nove branch z lokalne trackovanych
-Set-Location (Get-Location).Path
 git checkout Develop
 Write-Host "Switched back to Develop branch."
 
 Write-Host "Removing local tracking of branch $BranchName in Develop repository..."
-git branch --unset-upstream $BranchName 2>$null
 git branch -d $BranchName
 git fetch --prune
 
